ef551851674cb68ef3965382863350288cdc364b
roifrancisco <axel.sala@gmail.com>
2023-07-08 17:28:06 +0800

    initial commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am John Rey B. Calandria</span> I am a Basic Web Developer specializing in creating simple yet effective websites. My primary focus revolves around developing websites for my personal projects and activities. Through my work, you can witness the range of skills I possess and the progress I have made in my journey as a Basic Web Developer."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
